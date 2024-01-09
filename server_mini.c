756779f71dfac336bddaf0de5359659ff2dfff8c
nodokaha <scpeee@wail.ch>
2024-01-09 09:51:44 +0900

    add server_mini

--- server_mini.c
+++ server_mini.c
@@ -6,36 +6,23 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-int s;
-
-static void die(int signo)
-{
-  close(s);
-  exit(EXIT_SUCCESS);
-}
-
 int main(int argc, char *argv[])
 {
+  int s;
   struct sockaddr_in addr;
-  struct sigaction *act = calloc(sizeof(act), sizeof(struct sigaction *));
-
   char *buf = calloc(30, sizeof(char *));
-  act->sa_handler = die;
-
   strcpy(buf, "Hello world\n\0");
 
-  if((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) die(NULL);
+  if((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1);
 
   addr.sin_family = AF_INET;
   addr.sin_port = htons(atoi(argv[1]));
   addr.sin_addr.s_addr =  htonl(INADDR_ANY);
 
-  sigaction(SIGINT, act, NULL);
-
   while(1)
     {
       sleep(1);
-      if(sendto(s, buf, 30, 0, (struct sockaddr*)&addr, sizeof(addr)) == -1) die(NULL);
+      if(sendto(s, buf, 100, 0, (struct sockaddr*)&addr, sizeof(addr)) == -1);
     }
   close(s);
   return 0;
